594e5efd8ddaee4a3601dcd7409ec3dcbb78e0e4
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-05-13 23:37:18 +0530

    change Background Color when win

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -12,6 +11,9 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "⛔ No Number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number 🥳🎉";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess !== secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent =
