48419d0566c45f3304aefd6957ab2d9a69f97ccd
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-05-13 23:51:27 +0530

    Done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -24,4 +24,17 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You Lost the Game 😭😭";
     }
   }
+});
+
+//Again Btn
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
